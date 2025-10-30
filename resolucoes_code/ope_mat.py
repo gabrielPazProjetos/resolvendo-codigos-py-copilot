65a3fc333504a1aabaf7a6d3205f32e9e650f71c
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-30 12:06:51 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,11 +1,8 @@
-# Solicita dois números do usuário
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-# Solicita a operação desejada
 operacao = input("Digite a operação que deseja realizar (+, -, *, /): ")
 
-# Executa a operação com base na entrada do usuário
 if operacao == '+':
     resultado = num1 + num2
     print(f"Resultado da soma: {resultado}")
@@ -16,7 +13,6 @@ elif operacao == '*':
     resultado = num1 * num2
     print(f"Resultado da multiplicação: {resultado}")
 elif operacao == '/':
-    # Verifica se o divisor é diferente de zero
     if num2 != 0:
         resultado = num1 / num2
         print(f"Resultado da divisão: {resultado}")
